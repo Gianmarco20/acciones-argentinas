323c4f7c21a2e292236549af16d0a07e93d0c161
Gian <casertagianmarco19@gmail.com>
2022-05-13 12:41:16 -0300

    Initial Commit

---
+++ Acciones Argentinas App/Acciones_Argentinas_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acciones_Argentinas_AppApp.swift
+//  Acciones Argentinas App
+//
+//  Created by Gian Marco Caserta on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Acciones_Argentinas_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
