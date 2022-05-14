d38a108a94664a7746a42ca35423e5b9823fd682
Gian <casertagianmarco19@gmail.com>
2022-05-14 14:48:58 -0300

    - Mejore el cuadro con datos financieros de la compañía seleccionada en PerfilEmpresaView.

--- Acciones Argentinas App/Views/PerfilEmpresaView.swift
+++ Acciones Argentinas App/Views/PerfilEmpresaView.swift
@@ -34,31 +34,51 @@ struct PerfilEmpresaView: View {
                     ZStack {
                         Rectangle()
                             .foregroundColor(.white)
-                            .frame(height: 80, alignment: .center)
+                            .frame(height: 140, alignment: .center)
                             .cornerRadius(20)
                             .shadow(color: .gray, radius: 2, x: -2, y: 2)
-                        HStack(spacing: 20) {
-                            
-                            VStack {
-                                Text("Market cap")
-                                    .font(.headline)
-                                    .padding(.bottom, 2)
-                                Text("$ \(activo.currentStock!.marketCap)")
-                                    .font(.subheadline)
+                        VStack {
+                            HStack(spacing: 20) {
+                                
+                                VStack {
+                                    Text("Market cap")
+                                        .font(.headline)
+                                        .padding(.bottom, 2)
+                                    Text("$ \(activo.currentStock!.marketCap)")
+                                        .font(.subheadline)
+                                }
+                                VStack {
+                                    Text("Ingresos totales")
+                                        .font(.headline)
+                                        .padding(.bottom, 2)
+                                    Text("$ \(activo.currentStock!.ingresosTotales)")
+                                        .font(.subheadline)
+                                }
+                                VStack {
+                                    Text("Ingreso neto")
+                                        .font(.headline)
+                                        .padding(.bottom, 2)
+                                    Text("$ \(activo.currentStock!.ingresoNeto)")
+                                        .font(.subheadline)
+                                }
                             }
-                            VStack {
-                                Text("Ingresos totales")
-                                    .font(.headline)
-                                    .padding(.bottom, 2)
-                                Text("$ \(activo.currentStock!.ingresosTotales)")
-                                    .font(.subheadline)
-                            }
-                            VStack {
-                                Text("Ingreso neto")
-                                    .font(.headline)
-                                    .padding(.bottom, 2)
-                                Text("$ \(activo.currentStock!.ingresoNeto)")
-                                    .font(.subheadline)
+                            .padding(.bottom)
+                            HStack(spacing: 20) {
+                                
+                                VStack {
+                                    Text("BPA")
+                                        .font(.headline)
+                                        .padding(.bottom, 2)
+                                    Text("$ \(activo.currentStock!.bpa)")
+                                        .font(.subheadline)
+                                }
+                                VStack {
+                                    Text("EBITDA")
+                                        .font(.headline)
+                                        .padding(.bottom, 2)
+                                    Text("$ \(activo.currentStock!.ebitda)")
+                                        .font(.subheadline)
+                                }
                             }
                         }
                     }
